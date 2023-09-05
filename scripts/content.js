47b40deff15d6612c409ef28d6bf8da9235c4694
Jezzatator <jeremie.patot@protonmail.ch>
2023-09-05 16:16:15 +0200

    ajout de la fonction async et fetch et utilisation du localStorage

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,44 @@
+const fetchInsutls = async () => {
+    // Fetch pour récupéré le fichier JSON
+    try {
+        const response = await fetch("https://fr.wiktionary.org/w/api.php?action=query&list=categorymembers&cmtitle=Cat%C3%A9gorie:Insultes_en_fran%C3%A7ais&cmlimit=max&format=json");
+
+// Verifie si la réponse est OK - code HTTP 200
+        if (!response.ok) {
+            throw new Error("La requête a échoué avec un code HTTP ${response.status}");
+        }
+
+        const data = await response.json();
+
+        if (data.query && Array.isArray(data.query.categorymembers)) {
+            const titleArray = data.query.categorymembers.map(member => member.title);
+            //console.log("titleArray :" + titleArray)
+            localStorage.setItem('Insultes', JSON.stringify(titleArray));
+
+        } else {
+            console.error("le JSON ne contient pas la strucure attendu.");
+        }
+    } catch(error) {
+        console.error("une erreur s\'est produite lors de la récupération du JSON :", error)
+    }
+}
+
+const insultes = () => {
+    if (!(localStorage.getItem("Insultes"))) {
+        fetchInsutls()
+        return insultes()
+    } else {
+        return JSON.parse(localStorage.getItem("Insultes"))
+    }
+}
+
+
+console.log(insultes())
+
+
+// Fonction pour remplacer un mot spécifique 
+
+// D'abord cherche les balises à exclures
 function isExcluded(elm) {
     if (elm.tagName == "STYLE") {
       return true;
@@ -32,6 +73,7 @@ function isExcluded(elm) {
   
     }
   
+    //Si cela trouve une bonne balsie texte alors on fait le happy path
     if (elm.nodeType == Node.TEXT_NODE) {
   
       // exclude text node consisting of only spaces
@@ -39,8 +81,7 @@ function isExcluded(elm) {
         return
       }
   
-      // elm.nodeValue here is visible text we need.
-      console.log(elm.nodeValue);
+//ici le code pour remplacer les mots
 
       //Foreach insult 
       //Foreache INSULT/INSULTS/insults/Insult/Insults
